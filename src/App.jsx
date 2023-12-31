854d0dfae88fe593d2f0a8924b90678161ccb0d3
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2023-12-31 22:15:49 +0000

    new update with tailwindcss configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>12/31/23</h1>
+        <p>Coding for the last day last time in 2023. More codes in 2024</p>
+      </div>
+    </>
+  )
+}
+
+export default App
